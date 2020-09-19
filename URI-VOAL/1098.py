c3077a023c0bd9f365b6281e3530c3361da43728
asari <32879431+deomorxsy@users.noreply.github.com>
2020-09-19 10:03:56 -0300

    debugging URI 1098 and more comments

--- URI-VOAL/1098.py
+++ URI-VOAL/1098.py
@@ -18,7 +18,8 @@ while (index <= 2):
 	print("I={:.1f} J={:.1f}".format( float(coluna1) + somador_col2, (float(coluna2[index]) + somador_col2)))
 	index += 1
 
-while (accumulator <= 3):
+#AUTOMATED LOOP 1
+while (accumulator < 3):
 	somador_col2 += 0.2
 	index = 0
 	while (index <= 2):
@@ -28,8 +29,11 @@ while (accumulator <= 3):
 	#DEBUGGER: print("\nsomador_col2={}\naccumulator={}\n===========\n".format(somador_col2, accumulator)) 
 
 
-#coluna1 = str(somador_col2)
-#somador_col2 = somador
+somador_col2 += 0.2
+accumulator+=1
+# These two above are needed. If they were set to run one more loop, they would be equal to a string that represents a float.
+# And we need a string that represents an INT or an INT itself. BTW, this "one more loop" is represented by the logical operator ">=" in "index >= 3";
+# This, of course, represents 4 iterations, since we want our index starting at 0.
 
 #STOP and print int coluna1 and coluna2.
 index = 0
@@ -37,6 +41,7 @@ while (index <= 2):
 	print("I={} J={}".format( int(float(coluna1) + somador_col2), int((float(coluna2[index]) + somador_col2)) ))
 	index += 1
 
+'''
 
 accumulator=0
 
@@ -60,7 +65,7 @@ while (index <= 2):
 	print("I={} J={}".format( int(float(coluna1) + somador_col2), int((float(coluna2[index]) + somador_col2)) ))
 	index += 1
 
-
+'''
 '''
 Expected output from coluna1 (I):
 
