42f1d21cf91b3f8955f15520ab74f71eb5236c3d
asari <32879431+deomorxsy@users.noreply.github.com>
2020-09-21 20:43:10 -0300

    fixing 1099 2

--- URI-VOAL/1099.py
+++ URI-VOAL/1099.py
@@ -12,38 +12,70 @@ Output: Print the sum of all odd numbers between X and Y.
 
 numero_testes = int(input()); #para o for loop
 casos = []
-index = 0
-
 acumulador = []
 
 somatorio = 0
 
 for i in range(numero_testes):
 	casos = [int(i) for i in input().split(' ')]
+	somatorio=0
+	
+	while ((casos[0] == casos[1]) 
+		or (casos[0] + 1 == casos[1]) 
+		or (casos[1] + 1 == casos[0]) 
+		or (casos[0] == casos[1] + 2 and (casos[1]-1) % 2 == 0) 
+		or (casos[1] == casos[0] + 2 and (casos[0]-1) % 2 == 0)):
+
+		somatorio = 0
+		print("N1 Loop")
+		print("somatorio={}".format(somatorio))
+		print("somatorio depois da soma={}\n".format(somatorio))
+		acumulador.append(somatorio)
+		break
+	'''
+	somatorio = 0
+	while (casos[0] + 1 == casos[1]) or (casos[1] + 1 == casos[0]):
+		somatorio = 0
+		print("N2 Loop")
+		print("somatorio={}".format(somatorio))
+		print("somatorio depois da soma={}\n".format(somatorio))
+		acumulador.append(somatorio)
+		break
+	'''
+	somatorio = 0
 	while (casos[0] < casos[1]): 
 		for i in range(casos[0],casos[1]):
 			while (i % 2 != 0) and (i != casos[0]): #if
-				somatorio += i
+				print("N2 Loop")
+				print("somatorio={}".format(somatorio))
+				somatorio += i;
+				print("somatorio depois da soma={}\n".format(somatorio));
 				#print("{}".format(casos[i]))#DEBUGGER
 				break
 			acumulador.append(somatorio)
 		break
 	
 	somatorio=0
-
 	while (casos[0] > casos[1]): 
 		for i in range(casos[0],casos[1],-1): #start,stop,step
 			while (i % 2 != 0) and (i != casos[0]): #if
+				print("N3 Loop")
+				print("somatorio={}".format(somatorio))
 				somatorio += i
-				#print("{}".format(i)) # debugger
+				print("somatorio depois da soma={}\n".format(somatorio))
 				break
-			acumulador.append(somatorio)
 			#print("{}".format(i))#DEBUGGER
+			acumulador.append(somatorio)
 		#print("casos[0] não é maior que caso[1].") #DEBUGGER
 		break
 
+	
+
+
+print("=====================")
 for i in range(len(acumulador)):
 	print("{}".format(acumulador[i]))
+
 '''
 	if (casos[0] < casos[1]):
 		for i in range(len(casos)):
