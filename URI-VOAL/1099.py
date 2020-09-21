a0a152eb624ee6f91baee07d06154a8854b7d30c
asari <32879431+deomorxsy@users.noreply.github.com>
2020-09-21 18:13:37 -0300

    fixing URI 1099

--- URI-VOAL/1099.py
+++ URI-VOAL/1099.py
@@ -3,36 +3,60 @@
 '''
 Sum of Consecutive Odd Numbers II
 
-Read an integer N that is the number of test cases. Each test case is a line containing two integer numbers X and Y. Print the sum of all odd values between them, not including X and Y.
-Input
-
-The first line of input is an integer N that is the number of test cases that follow. Each test case is a line containing two integer X and Y.
-Output
-
-Print the sum of all odd numbers between X and Y.
+Read an integer N that is the number of test cases. Each test case is a line containing two
+ integer numbers X and Y. Print the sum of all odd values between them, not including X and Y.
+Input: The first line of input is an integer N that is the number of test cases that follow.
+ Each test case is a line containing two integer X and Y.
+Output: Print the sum of all odd numbers between X and Y.
 '''
 
-teste = input();
-accumulator = '';
-index_finder = 0;
+numero_testes = int(input()); #para o for loop
+casos = []
+index = 0
 
-comeco = 0; #N
-fim = 0; #Y
+acumulador = []
 
-for i in range(len(teste) - 1):
-	#if teste[i] == ' ':
-		#print("Espaço está em {}".format(i))
-	while (teste[i] == ' '):
-		index_finder = i;
-		break;
-	#range(len(teste)) vai retornar 5. Porém o index em strings e listas começa em 0. Por isso o -1.
-	#Deixar sem o -1 retorna out of range.
-	#O for loop escaneará todos os indexes até encontrar a condição prevista no while loop.
-	#Uma vez atendida, a condição fará com que index_finder, que antes era 0 (int), receba como atribuição i,
-	#que é o valor do index.
-	#O break encerra ambos os loops.
+somatorio = 0
 
-comeco = int(teste[0:index_finder])
-fim = int(teste[index_finder+1:])
-print("accumulator={}".format(accumulator))
-print("index_finder = {}".format(index_finder))
+for i in range(numero_testes):
+	casos = [int(i) for i in input().split(' ')]
+	while (casos[0] < casos[1]): 
+		for i in range(casos[0],casos[1]):
+			while (i % 2 != 0) and (i != casos[0]): #if
+				somatorio += i
+				#print("{}".format(casos[i]))#DEBUGGER
+				break
+			acumulador.append(somatorio)
+		break
+	
+	somatorio=0
+
+	while (casos[0] > casos[1]): 
+		for i in range(casos[0],casos[1],-1): #start,stop,step
+			while (i % 2 != 0) and (i != casos[0]): #if
+				somatorio += i
+				#print("{}".format(i)) # debugger
+				break
+			acumulador.append(somatorio)
+			#print("{}".format(i))#DEBUGGER
+		#print("casos[0] não é maior que caso[1].") #DEBUGGER
+		break
+
+for i in range(len(acumulador)):
+	print("{}".format(acumulador[i]))
+'''
+	if (casos[0] < casos[1]):
+		for i in range(len(casos)):
+			print("{}".format(casos[i]))
+	else:
+		print("casos[0] não é maior que caso[1].")
+
+#for item in range(casos[i][0],casos[i][1],-1):
+'''
+'''
+index=0
+while (index <= numero_testes):
+	casos = input().split('')
+	index+=1
+
+'''
--- URI-VOAL/teste2.py
+++ URI-VOAL/teste2.py
@@ -1,15 +1,8 @@
-#!/usr/bin/python3
-
-
-nf = int(input()) #numero de funcionario
-nh = int(input()) #horas trabalhadas
-vh = float(input()) #valor/hora
-sl = float(nh * vh) #salario
-
-print('NUMBER = '.format(nf))
-print('SALARY = U$ {:.2f}'.format(sl))
-
-'''
-print('NUMBER = {}',nf)
-print('SALARY = U$ %.2f'%sl)
-'''
+I=int(1)
+J=int(7)
+while(I<10):
+    while(J>4):
+        print('I={} J={}'. format(I,J))
+        J=J-1
+    I=I+2
+    J=J+5
