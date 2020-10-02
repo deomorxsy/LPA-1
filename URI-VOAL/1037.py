18b9e5e86f2e45000a76f5eeed5042f9717a7e04
asari <32879431+deomorxsy@users.noreply.github.com>
2020-10-02 00:10:16 -0300

    adding 1037

---
+++ URI-VOAL/1037.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+entrada = float(input())
+
+intervalos_str = ["[0,25]", "(25,50]", "(50,75]", "(75,100]"]
+intervalos = [[0,25], [25,50], [50,75], [75,100]]
+
+if entrada >= 0 and entrada <= 25:
+	print("Intervalo {}".format(intervalos_str[0]))
+
+elif entrada > 25 and entrada <= 50:
+	print("Intervalo {}".format(intervalos_str[1]))
+
+elif entrada > 50 and entrada <= 75:
+	print("Intervalo {}".format(intervalos_str[2]))
+
+elif entrada > 75 and entrada <= 100:
+	print("Intervalo {}".format(intervalos_str[3]))
+
+elif entrada < 0 or entrada > 100:
+	print("Fora de intervalo")
