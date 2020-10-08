b54d24a5a869c9a7a5789e96b5d4a7ceed4c8398
asari <32879431+deomorxsy@users.noreply.github.com>
2020-10-08 00:34:57 -0300

    deleting unused variable on 1050

--- extras/1050.py
+++ extras/1050.py
@@ -2,9 +2,7 @@
 
 ddd = [61, 71, 11, 21, 32, 19, 27, 31]
 destination = ["Brasilia", "Salvador", "Sao Paulo", "Rio de Janeiro", "Juiz de Fora", "Campinas", "Vitoria", "Belo Horizonte"]
-
 entrada = int(input())
-saida = ""
 
 for i in range(len(ddd)-1):
 	if entrada == ddd[i]:
